b1c5a2534c825780b9d290d7a4f96d58d3d54882
Elias Fazel <elias.fazel@gmail.com>
2023-01-04 08:54:02 -0800

    [Phone][ChangeLog] ~ Firebase Analytics/Crashlytics Added
    [Build][ChangeLog]
    # Libraries Updated
    # Dependencies Updated

--- huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/4/23, 8:12 AM
+ * Last modified 1/4/23, 8:48 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
--- huehue/lib/gameplay/hue_to_hue.dart
+++ huehue/lib/gameplay/hue_to_hue.dart
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/4/23, 8:19 AM
+ * Last modified 1/4/23, 8:53 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
@@ -11,6 +11,7 @@
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:huehue/preferences/io/preferences_io.dart';
 import 'package:huehue/resources/colors_resources.dart';
 import 'package:huehue/resources/strings_resources.dart';
 import 'package:huehue/utils/animation/fade_transition.dart';
@@ -19,6 +20,7 @@ import 'package:huehue/utils/operations/colors.dart';
 import 'package:huehue/utils/operations/numbers.dart';
 import 'package:huehue/utils/ui/system_bars.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:widget_mask/widget_mask.dart';
 
 class HueToHue extends StatefulWidget {
 
@@ -29,6 +31,8 @@ class HueToHue extends StatefulWidget {
 }
 
 class _HueToHueState extends State<HueToHue> with TickerProviderStateMixin  {
+
+  PreferencesIO preferencesIO = PreferencesIO();
 
   List<Color> allColors = [
     ColorsResources.lightestCyan,
@@ -49,17 +53,25 @@ class _HueToHueState extends State<HueToHue> with TickerProviderStateMixin  {
 
   int animationDuration = 7777;
 
-  Widget gameplayPlaceholder = Container(
-    height: 399,
-    width: 351,
-    alignment: Alignment.center,
-    child: LoadingAnimationWidget.discreteCircle(
-      color: ColorsResources.premiumLight,
-      secondRingColor: ColorsResources.primaryColorLighter,
-      thirdRingColor: ColorsResources.cyan,
-      size: 73
+  Widget gameplayPlaceholder = Center(
+    child: Container(
+        height: 333,
+        width: 333,
+        alignment: Alignment.center,
+        child: LoadingAnimationWidget.discreteCircle(
+            color: ColorsResources.premiumLight,
+            secondRingColor: ColorsResources.primaryColorLighter,
+            thirdRingColor: ColorsResources.cyan,
+            size: 73
+        )
     )
   );
+
+  int levels = 0;
+  double levelsOpacity = 0.0;
+
+  int points = 0;
+  double pointsOpacity = 0.0;
 
   bool aInterceptor(bool stopDefaultButtonEvent, RouteInfo info) {
 
@@ -76,6 +88,8 @@ class _HueToHueState extends State<HueToHue> with TickerProviderStateMixin  {
     changeColor(ColorsResources.primaryColorDarkest, ColorsResources.primaryColorDarkest);
 
     super.initState();
+
+    initializeGameInformation();
 
     retrieveGameData();
 
@@ -189,7 +203,241 @@ class _HueToHueState extends State<HueToHue> with TickerProviderStateMixin  {
                 body: SizedBox(
                     height: double.maxFinite,
                     width: double.maxFinite,
-                    child: gameplayPlaceholder
+                    child: Stack(
+                      children: [
+
+                        gameplayPlaceholder,
+
+                        /* Start - Level */
+                        Positioned(
+                            bottom: 37,
+                            right: 37,
+                            child: AnimatedOpacity(
+                              opacity: levelsOpacity,
+                              duration: const Duration(milliseconds: 1357),
+                              child: Container(
+                                  decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: ColorsResources.black.withOpacity(0.73),
+                                            blurRadius: 19
+                                        )
+                                      ]
+                                  ),
+                                  child: const WidgetMask(
+                                      blendMode: BlendMode.srcATop,
+                                      childSaveLayer: true,
+                                      mask: ColoredBox(
+                                          color: ColorsResources.primaryColorDarkest
+                                      ),
+                                      child: Image(
+                                        image: AssetImage("squircle.png"),
+                                        height: 73,
+                                        width: 73,
+                                      )
+                                  )
+                              )
+                            )
+                        ),
+                        Positioned(
+                            bottom: 37,
+                            right: 37,
+                            child: AnimatedOpacity(
+                              opacity: levelsOpacity,
+                              duration: const Duration(milliseconds: 1357),
+                              child: WidgetMask(
+                                blendMode: BlendMode.srcIn,
+                                childSaveLayer: true,
+                                mask: Material(
+                                    shadowColor: Colors.transparent,
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                        splashColor: ColorsResources.primaryColor,
+                                        splashFactory: InkRipple.splashFactory,
+                                        onTap: () {
+
+
+
+                                        },
+                                        child: SizedBox(
+                                            height: 73,
+                                            width: 73,
+                                            child: Center(
+                                                child: Padding(
+                                                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                                    child: Text(
+                                                      levels.toString(),
+                                                      textAlign: TextAlign.center,
+                                                      style: TextStyle(
+                                                          color: ColorsResources.premiumLight,
+                                                          fontSize: 31,
+                                                          fontFamily: "Electric",
+                                                          shadows: [
+                                                            Shadow(
+                                                                color: ColorsResources.white.withOpacity(0.19),
+                                                                blurRadius: 7,
+                                                                offset: const Offset(0, 3)
+                                                            )
+                                                          ]
+                                                      ),
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                ),
+                                child: const Image(
+                                  image: AssetImage("squircle.png"),
+                                  height: 73,
+                                  width: 73,
+                                ),
+                              )
+                            )
+                        ),
+                        Positioned(
+                            bottom: 119,
+                            right: 37,
+                            child: AnimatedOpacity(
+                              opacity: levelsOpacity,
+                              duration: const Duration(milliseconds: 1357),
+                              child: SizedBox(
+                                  width: 73,
+                                  child: Container(
+                                      decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: ColorsResources.black.withOpacity(0.51),
+                                                blurRadius: 13,
+                                                offset: const Offset(0, 3)
+                                            )
+                                          ]
+                                      ),
+                                      child: const Image(
+                                        image: AssetImage("level_indicator_bottom.png"),
+                                        fit: BoxFit.contain,
+                                      )
+                                  )
+                              )
+                            )
+                        ),
+                        /* End - Level */
+
+                        /* Start - Point */
+                        Positioned(
+                            top: 37,
+                            left: 37,
+                            child: AnimatedOpacity(
+                                opacity: pointsOpacity,
+                                duration: const Duration(milliseconds: 1357),
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                        boxShadow: [
+                                          BoxShadow(
+                                              color: ColorsResources.black.withOpacity(0.73),
+                                              blurRadius: 19
+                                          )
+                                        ]
+                                    ),
+                                    child: const WidgetMask(
+                                        blendMode: BlendMode.srcATop,
+                                        childSaveLayer: true,
+                                        mask: ColoredBox(
+                                            color: ColorsResources.primaryColorDarkest
+                                        ),
+                                        child: Image(
+                                          image: AssetImage("squircle.png"),
+                                          height: 73,
+                                          width: 73,
+                                        )
+                                    )
+                                )
+                            )
+                        ),
+                        Positioned(
+                            top: 37,
+                            left: 37,
+                            child: AnimatedOpacity(
+                                opacity: pointsOpacity,
+                                duration: const Duration(milliseconds: 1357),
+                                child: WidgetMask(
+                                  blendMode: BlendMode.srcIn,
+                                  childSaveLayer: true,
+                                  mask: Material(
+                                      shadowColor: Colors.transparent,
+                                      color: Colors.transparent,
+                                      child: InkWell(
+                                          splashColor: ColorsResources.primaryColor,
+                                          splashFactory: InkRipple.splashFactory,
+                                          onTap: () {
+
+
+
+                                          },
+                                          child: SizedBox(
+                                              height: 73,
+                                              width: 73,
+                                              child: Center(
+                                                  child: Padding(
+                                                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                                      child: Text(
+                                                        points.toString(),
+                                                        textAlign: TextAlign.center,
+                                                        style: TextStyle(
+                                                            color: ColorsResources.premiumLight,
+                                                            fontSize: 31,
+                                                            fontFamily: "Electric",
+                                                            shadows: [
+                                                              Shadow(
+                                                                  color: ColorsResources.white.withOpacity(0.19),
+                                                                  blurRadius: 7,
+                                                                  offset: const Offset(0, 3)
+                                                              )
+                                                            ]
+                                                        ),
+                                                      )
+                                                  )
+                                              )
+                                          )
+                                      )
+                                  ),
+                                  child: const Image(
+                                    image: AssetImage("squircle.png"),
+                                    height: 73,
+                                    width: 73,
+                                  ),
+                                )
+                            )
+                        ),
+                        Positioned(
+                            top: 119,
+                            left: 37,
+                            child: AnimatedOpacity(
+                                opacity: pointsOpacity,
+                                duration: const Duration(milliseconds: 1357),
+                                child: SizedBox(
+                                    width: 73,
+                                    child: Container(
+                                        decoration: BoxDecoration(
+                                            boxShadow: [
+                                              BoxShadow(
+                                                  color: ColorsResources.black.withOpacity(0.51),
+                                                  blurRadius: 13,
+                                                  offset: const Offset(0, 3)
+                                              )
+                                            ]
+                                        ),
+                                        child: const Image(
+                                          image: AssetImage("point_indicator.png"),
+                                          fit: BoxFit.contain,
+                                        )
+                                    )
+                                )
+                            )
+                        ),
+                        /* End - Point */
+
+                      ],
+                    )
                 )
             )
         )
@@ -235,6 +483,34 @@ class _HueToHueState extends State<HueToHue> with TickerProviderStateMixin  {
             ),
           )
       );
+
+    });
+
+  }
+
+  void initializeGameInformation() async {
+
+    preferencesIO.retrieveContinuously().then((value) => {
+
+      if (value) {
+
+        setState(() {
+
+
+          levelsOpacity = 1.0;
+
+
+        })
+
+      }
+
+    });
+
+    setState(() {
+
+
+      pointsOpacity = 1.0;
+
 
     });
 
