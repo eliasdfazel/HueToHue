75fccb59e4eb0fb71acfc87cde1543018ffa19ac
Elias Fazel <elias.fazel@gmail.com>
2023-01-05 09:00:22 -0800

    [Phone][ChangeLog] ~ Firebase Analytics/Crashlytics Added
    [Build][ChangeLog]
    # Libraries Updated
    # Dependencies Updated

--- huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright © 2023 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 1/5/23, 9:00 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 package io.flutter.plugins;
 
 import androidx.annotation.Keep;
