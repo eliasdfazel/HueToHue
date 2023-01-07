ef3a8fe51ed45e470d835340dab63784fe2048de
Elias Fazel <elias.fazel@gmail.com>
2023-01-07 07:21:38 -0800

    [Phone][ChangeLog]
    [Build][ChangeLog]
    # Libraries Updated
    # Dependencies Updated

--- huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/5/23, 9:34 AM
+ * Last modified 1/7/23, 7:17 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
