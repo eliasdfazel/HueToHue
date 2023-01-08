434b8b311719ed601489b822fdb7006ea120172f
Elias Fazel <elias.fazel@gmail.com>
2023-01-08 03:37:33 -0800

    [Phone][ChangeLog] ~ Syncing Process Added
    [Build][ChangeLog]
    # Libraries Updated
    # Dependencies Updated

--- huehue/lib/gameplay/scenes/elements/gradients_shapes.dart
+++ huehue/lib/gameplay/scenes/elements/gradients_shapes.dart
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/7/23, 8:32 AM
+ * Last modified 1/7/23, 8:34 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
