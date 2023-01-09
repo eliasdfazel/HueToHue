9159dd5e4f0b22fe5b1e1bd64ab20c30936cde7f
Elias Fazel <elias.fazel@gmail.com>
2023-01-09 08:13:00 -0800

    [Phone][ChangeLog] ~ Sounds Added with Downloading Process from Server
    [Build][ChangeLog]
    # Libraries Updated
    # Dependencies Updated

--- huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ huehue/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -2,7 +2,7 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 1/9/23, 8:08 AM
+ * Last modified 1/9/23, 8:10 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
