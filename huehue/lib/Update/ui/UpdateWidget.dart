11504d360a87e1027880812d4fd9507a89899d40
Elias Fazel <elias.fazel@gmail.com>
2024-03-27 13:05:13 -0400

    [Phone][ChangeLog] ~ IQ Difficulty
    [Build][ChangeLog]
    # Libraries Updated
    # Dependencies Updated

--- huehue/lib/Update/ui/UpdateWidget.dart
+++ huehue/lib/Update/ui/UpdateWidget.dart
@@ -16,49 +16,52 @@ import 'package:url_launcher/url_launcher.dart';
 
 Widget updateWidget(String applicationLink) {
 
-  return Material(
-      shadowColor: Colors.transparent,
-      color: Colors.transparent,
-      child: InkWell(
-          splashColor: ColorsResources.lightGreen.withOpacity(0.73),
-          splashFactory: InkRipple.splashFactory,
-          onTap: () {
+  return Container(
+      height: double.infinity,
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                ColorsResources.primaryColor,
+                ColorsResources.primaryColorDarker
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+          )
+      ),
+    child: Material(
+        shadowColor: Colors.transparent,
+        color: Colors.transparent,
+        child: InkWell(
+            splashColor: ColorsResources.lightGreen.withOpacity(0.73),
+            splashFactory: InkRipple.splashFactory,
+            onTap: () {
 
-            Future.delayed(const Duration(milliseconds: 555), () {
+              Future.delayed(const Duration(milliseconds: 555), () {
 
-              launchUrl(Uri.parse(applicationLink), mode: LaunchMode.externalApplication);
+                launchUrl(Uri.parse(applicationLink), mode: LaunchMode.externalApplication);
 
-            });
+              });
 
-          },
-          child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  ColorsResources.primaryColor,
-                  ColorsResources.primaryColorDarker
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight
-              )
-            ),
-            child: SizedBox(
-              height: 333,
-              width: 333,
-              child: Center(
-                child: Text(
-                  StringsResources.availableUpdate(),
-                  style: const TextStyle(
-                    color: ColorsResources.premiumLight,
-                    fontSize: 37,
-                    fontFamily: 'Electric'
+            },
+            child: Center(
+              child: SizedBox(
+                  height: 333,
+                  width: 333,
+                  child: Center(
+                      child: Text(
+                          StringsResources.availableUpdate(),
+                          style: const TextStyle(
+                              color: ColorsResources.premiumLight,
+                              fontSize: 37,
+                              fontFamily: 'Electric'
+                          )
+                      )
                   )
-                )
               )
             )
-          )
-      )
+        )
+    )
   );
+
 }
